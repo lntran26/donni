eea3a8bb4d0fa73f737318443af98589783a402b
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-03-08 19:36:39 -0700

    update two benchmarking scripts for hpc
    one script (parallel) is to output plotable results, one script (jobarray) is to print out run time for each job

--- bin/benchmarking/benchmarking_jobarray.py
+++ bin/benchmarking/benchmarking_jobarray.py
@@ -9,37 +9,11 @@ import sys,os
 
 if __name__ == '__main__': 
     # import test sets previously generated for 2D-split-migration
-    list_test_dict = pickle.load(open(
-        '/groups/rgutenk/lnt/projects/ml-dadi/data/test-data-corrected','rb'))
-    # randomly select 5 datasets from each variance case
-    test_data = {}
-    for test_dict in list_test_dict:
-        for i in range(5):
-            params, fs = random.choice(list(test_dict.items()))
-            # pick a different set until find a unique param value
-            while params in test_data: 
-                params, fs = random.choice(list(test_dict.items()))
-            # once find a unique params, escape while loop and add that set 
-            # scale by theta=1000 for a more realistic fs
-            test_data[params] = fs*1000
-
-    # import trained RFR
-    list_rfr = pickle.load(open(
-                '/groups/rgutenk/lnt/projects/ml-dadi/data/list-rfr','rb'))
-    #  use rfr to give predictions for test_data
-    list_pred = []
-    list_key = [] # list of key (param) to get fs from test_data dict
-    for rfr in list_rfr:
-        y_true, y_pred = util.rfr_test(rfr, test_data)
-        # perform log transform on predictionr results and 
-        # convert p in y_pred from np.array to list format
-        y_pred_transform = []
-        for p in y_pred:
-            y_pred_transform.append([10**p[0], 10**p[1], p[2], p[3]])
-        list_pred.append(y_pred_transform)
-        # also copy y_true to get keys
-        list_key.append(deepcopy(y_true))
-    # format:list_pred=[[20 np.arrays of 20 pred_param;theta1],[],[],[theta10k]]
+    test_set = pickle.load(open(
+        '/groups/rgutenk/lnt/projects/ml-dadi/data/benchmarking_test_set','rb'))
+    p_true_list = [test_set[i][0] for i in range(20)]
+    fs_list = [test_set[i][1] for i in range(20)]
+    p0_list = [test_set[i][2] for i in range(60)]
 
     # benchmarking
     # common process: set up dadi
@@ -49,40 +23,6 @@ if __name__ == '__main__':
     pts_l = [40, 50, 60]
     func_ex = dadi.Numerics.make_extrap_func(func)
     lb, ub = [1e-2,1e-2,0.1,1], [1e2,1e2,2,10]
-
-    # 1 list of fs, 1 list of p_true
-    fs_list, p_true_list = [], []
-    # 3 lists of different starting point p0
-    p1_list, p2_list, p3_list = [],[],[]
-    
-    for i in range(20):
-        # input each fs from test_data to infer from
-        p = list_key[0][i] # true params
-        p_transform = [10**p[0], 10**p[1], p[2], p[3]]
-        p_true_list.append(p_transform)
-
-        fs = test_data[p]
-        fs_list.append(fs)
-        
-        # List of generic starting points
-        p1_list.append([1, 1, 0.95, 4.5])
-
-        # List of starting points from RFR theta 1 predictions
-        sel_params_RFR_1 = list_pred[0][i]
-        p2_list.append(sel_params_RFR_1)
-
-        # List of starting points from average 4 RFRs prediction
-        arr = np.array([list_pred[0][i],list_pred[1][i],
-                        list_pred[2][i],list_pred[3][i]])
-        sel_params_RFR_avg = np.mean(arr, axis=0).tolist()
-        p3_list.append(sel_params_RFR_avg)
-
-    # to make it easier for job array submission
-    # append all three lists together into one list of all starting points
-    p0_list = p1_list + p2_list + p3_list
-    # extend fs_list and p_true_list 2 times to match length
-    # fs_list_ext = fs_list * 3
-    # p_true_list_ext = p_true_list * 3
 
     if 'SLURM_SUBMIT_DIR' in os.environ:
     # Set module search path so it will search in qsub directory
@@ -113,30 +53,16 @@ if __name__ == '__main__':
     print("Optimized params:",[round(p,5) for p in popt])
     print("Model Max Log Likelihood:", round(llnlopt,5))   
 
-    # make lists to save the results
-    # results_list = []
-    # p1_results_list, p2_results_list, p3_results_list = [], [], []
-
     # THREE DIFFERENT STARTING POINTS: print out results
     # 1. Generic starting point for regular dadi procedure
     if process_id < 20:
         print('Optimization time for dadi only: {0:.2f}s'.format(end),'\n') 
-        # p1_results_list.append(popt)
 
     # 2. Starting with Prediction from rfr trained on theta=1
     if process_id >= 20 and process_id < 40:
         print('Optimization time for dadi + RFR_1: {0:.2f}s'.format(end),'\n')
-        # p2_results_list.append(popt)
 
     # 3. Average prediction from all four RFR trained on different thetas
     if process_id >= 40:    
         print('Optimization time for dadi + 4_RFR_avg: {0:.2f}s'.format(end),
                                                                         '\n')
-        # p3_results_list.append(popt)
-
-    # for i in range(20):
-    #   results_list.append((p_true_list[i], p1_results_list[i],
-        #            p2_results_list[i], p3_results_list[i]))
-
-    # pickle.dump(results_list, open(
-     #   '/groups/rgutenk/lnt/projects/ml-dadi/results/benchmark_out', 'wb'), 2)
--- bin/benchmarking/benchmarking_parallel.py
+++ bin/benchmarking/benchmarking_parallel.py
@@ -26,48 +26,24 @@ def dadi_opt_parallel(p_true_list, p_list, fs_list, func, pts_l, lb, ub, ncpu=No
     with Pool(processes=ncpu) as pool:
             opt_list = pool.map(worker_func_opt, args_list)
     
-    for p_true, opt in zip(p_true_list, opt_list):
-        print("True params:", [round(val,5) for val in p_true])
-        print("True Max Log Likelihood:", 
-                    round(dadi.Inference.ll_multinom(fs, fs),5))
-        print("Optimized params:",[round(p,5) for p in opt[0]])
-        print("Max Log Likelihood:", round(opt[1],5), '\n')
+    # for p_true, opt in zip(p_true_list, opt_list):
+    #     print("True params:", [round(val,5) for val in p_true])
+    #     print("True Max Log Likelihood:", 
+    #                 round(dadi.Inference.ll_multinom(fs, fs),5))
+    #     print("Optimized params:",[round(p,5) for p in opt[0]])
+    #     print("Max Log Likelihood:", round(opt[1],5), '\n')
+
+    return opt_list
 
 if __name__ == '__main__': 
-    # import test sets previously generated for 2D-migration
-    list_test_dict = pickle.load(open('../../data/2d-splitmig/test-data-corrected','rb'))
-    # randomly select 5 datasets from each variance case
-    test_data = {}
-    for test_dict in list_test_dict:
-        for i in range(5):
-            params, fs = random.choice(list(test_dict.items()))
-            # pick a different set until find a unique param value
-            while params in test_data: 
-                params, fs = random.choice(list(test_dict.items()))
-            # once find a unique params, escape while loop and add that set 
-            # to test_data dict
-            # test_data[params] = fs
-            # more realistic data option--> scale fs by 1000
-            test_data[params] = fs*1000
-
-    # import trained RFR
-    list_rfr = pickle.load(open('../../data/2d-splitmig/list-rfr','rb'))
-    #  use rfr to give predictions for test_data
-    list_pred = []
-    list_key = [] # list of key (param) to get fs from test_data dict
-    for rfr in list_rfr:
-        y_true, y_pred = util.rfr_test(rfr, test_data)
-        # perform log transform on predictionr results and 
-        # convert p in y_pred from np.array to list format
-        y_pred_transform = []
-        for p in y_pred:
-            y_pred_transform.append([10**p[0], 10**p[1], p[2], p[3]])
-        list_pred.append(y_pred_transform)
-        # also copy y_true to get keys
-        list_key.append(deepcopy(y_true))
+    test_set = pickle.load(open('benchmarking_test_set','rb'))
+    p_true_list = [test_set[i][0] for i in range(20)]
+    fs_list = [test_set[i][1] for i in range(20)]
+    p1_list = [test_set[i][2] for i in range(20)]
+    p2_list = [test_set[i][2] for i in range(20, 40)]
+    p3_list = [test_set[i][2] for i in range(40, 60)]
 
     # benchmarking: parallel
-    
     # common process: set up dadi
     # designate demographic model, sample size, bounds, extrapolation grid 
     func = dadi.Demographics2D.split_mig
@@ -75,49 +51,19 @@ if __name__ == '__main__':
     pts_l = [40, 50, 60]
     lb = [1e-2,1e-2,0.1,1]
     ub = [1e2,1e2,2,10]
-    
-    fs_list, p_true_list = [], []
-    p1_list, p2_list, p3_list = [],[],[]
-    
-    for i in range(2): # change to increase # of test case run
-        # input each fs from test_data to infer from
-        p = list_key[0][i] # true params
-        p_transform = [10**p[0], 10**p[1], p[2], p[3]]
-        p_true_list.append(p_transform)
-
-        fs = test_data[p]
-        fs_list.append(fs)
-        
-        # List of generic starting points
-        p1_list.append([1, 1, 0.95, 4.5])
-
-        # List of starting points from RFR theta 1 predictions
-        sel_params_RFR_1 = list_pred[0][i]
-        p2_list.append(sel_params_RFR_1)
-
-        # List of starting points from average 4 RFRs prediction
-        arr = np.array([list_pred[0][i],list_pred[1][i],
-                        list_pred[2][i],list_pred[3][i]])
-        sel_params_RFR_avg = np.mean(arr, axis=0).tolist()
-        p3_list.append(sel_params_RFR_avg)
-
 
     # THREE DIFFERENT STARTING POINTS
     # 1. Generic starting point for regular dadi procedure
-    print('\n',"####################",'\n')
-    print("CASE 1: dadi only")
-    start = time.time()
-    dadi_opt_parallel(p_true_list, p1_list, fs_list, func, pts_l, lb, ub)
-    print('Optimization time for dadi: {0:.2f}s'.format(time.time()-start))
+    p1_opt = dadi_opt_parallel(p_true_list, p1_list, fs_list, func, pts_l, lb, ub)
     
     # 2. Starting with Prediction from rfr trained on theta=1
-    print("CASE 2: dadi + RFR_1")
-    start = time.time()
-    dadi_opt_parallel(p_true_list, p2_list, fs_list, func, pts_l, lb, ub)
-    print('Optimization time for dadi + RFR_1: {0:.2f}s'.format(time.time()-start))
+    p2_opt = dadi_opt_parallel(p_true_list, p2_list, fs_list, func, pts_l, lb, ub)
 
     # 3. Average prediction from all four RFR trained on different thetas    
-    print("CASE 3: dadi + 4_RFR_avg")
-    start = time.time()
-    dadi_opt_parallel(p_true_list, p3_list, fs_list, func, pts_l, lb, ub)
-    print('Optimization time for dadi + 4_RFR_avg: {0:.2f}s'.format(time.time()-start))
+    p3_opt = dadi_opt_parallel(p_true_list, p3_list, fs_list, func, pts_l, lb, ub)
+
+    results_list = []
+    for i in range(20):
+        results_list.append((p_true_list[i], p1_opt[i], p2_opt[i], p3_opt[i]))
+
+    pickle.dump(results_list, open('benchmarking_results_set', 'wb'), 2)
