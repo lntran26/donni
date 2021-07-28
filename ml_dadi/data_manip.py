dfe2ba01844e7909be8efdec15c6d83e4151616c
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-07-27 21:34:02 -0400

    Update data_manip.py
    remove redundant functions, combine all logs/bootstrap generating data into one, update bootstrapping functions

--- ml_dadi/data_manip.py
+++ ml_dadi/data_manip.py
@@ -10,16 +10,24 @@ def worker_func(args):
     func_ex = dadi.Numerics.make_extrap_func(func)
     return func_ex(p, ns, pts_l)
 
-def generating_data_parallel(params_list, theta_list, 
-                                func, ns, pts_l, ncpu=None):
-    '''Parallelized version for generating_data using multiprocessing.
+def generating_data(params_list, theta_list, func, ns, pts_l,
+                        logs=None, bootstrap=False, ncpu=None):
+    '''Parallelized version for generating data using multiprocessing.
     If npcu=None, it will use all the CPUs on the machine. 
     Otherwise user can specify a limit.
+    logs is a list where len(logs) = len(p) and 
+    logs[i] = True if p[i] is in log scale. 
+    If bootstrap is True, will generate data for bootstrapping purpose.
     Returns a list of dictionaries where each dictionary stores
-    a data set for training or testing RFR algorithm. Dictionaries
-    have structure params:fs.
+    a data set for training or testing RFR algorithm. 
+    Dictionaries have structure params:fs.
     '''
-    arg_list = [(p, func, ns, pts_l) for p in params_list]
+    if logs != None:
+        arg_list =[([10**p[i] if logs[i] else p[i] for i in range(len(logs))],
+                        func, ns, pts_l) for p in params_list]
+    else:
+        arg_list = [(p, func, ns, pts_l) for p in params_list]
+    
     with Pool(processes=ncpu) as pool:
             fs_list = pool.map(worker_func, arg_list)
             
@@ -27,156 +35,24 @@ def generating_data_parallel(params_list, theta_list,
     for theta in theta_list:
         data_dict = {}
         for params, fs in zip(params_list, fs_list):
-            fs.flat[0] = 0
-            fs.flat[-1] = 0
-            if theta == 1:
-                fs_tostore = fs
-            else:
+            if bootstrap:# only run this if want to generate bootstrap data sets
                 fs_tostore = (theta*abs(fs)).sample()
-            if fs_tostore.sum()==0:
-                pass
-            else:    
-                data_dict[params] = fs_tostore/fs_tostore.sum()                
+                data_dict[params] = [fs_tostore, []]
+                for i in range(200):# 200 bootstrap samples for each fs
+                    data_dict[params][1].append(fs_tostore.sample())
+            else:# generate data for ML training and testing        
+                fs.flat[0] = 0
+                fs.flat[-1] = 0
+                if theta == 1:
+                    fs_tostore = fs
+                else:
+                    fs_tostore = (theta*abs(fs)).sample()
+                if fs_tostore.sum()==0:
+                    pass
+                else:    
+                    data_dict[params]=fs_tostore/fs_tostore.sum()                
         list_dicts.append(data_dict)
     return list_dicts
-
-# ! log-scale params, define logs
-# repeat of above functions, except some params have been log-transformed
-# logs is a list where len(logs) = len(p) and logs[i] = True if p[i] is 
-# in log scale 
-def worker_func_log(args):
-    (p, func, ns, pts_l, logs) = args
-    func_ex = dadi.Numerics.make_extrap_func(func)
-    return func_ex([10**p[i] if logs[i] else p[i] for i in range(len(logs))],
-                   ns, pts_l)
-                   
-# ! log-scale params, define logs
-# takes extra argument logs, as defined above
-def generating_data_parallel_log(params_list, theta_list, 
-                                func, ns, pts_l, logs, ncpu=None):
-    '''Parallelized version for generating_data using multiprocessing.
-    If npcu=None, it will use all the CPUs on the machine. 
-    Otherwise user can specify a limit.
-    logs is a list specifying the indices of params that are in log scale
-    Returns a list of dictionaries where each dictionary stores
-    a data set for training or testing RFR algorithm. Dictionaries
-    have structure params:fs.
-    '''
-    arg_list = [(p, func, ns, pts_l, logs) for p in params_list]
-    with Pool(processes=ncpu) as pool:
-            fs_list = pool.map(worker_func_log, arg_list)
-
-    list_dicts = []
-    for theta in theta_list:
-        data_dict = {}
-        for params, fs in zip(params_list, fs_list):
-            fs.flat[0] = 0
-            fs.flat[-1] = 0
-            if theta == 1:
-                fs_tostore = fs
-            else:
-                fs_tostore = (theta*abs(fs)).sample()
-            if fs_tostore.sum()==0:
-                pass
-            else:    
-                data_dict[params] = fs_tostore/fs_tostore.sum()           
-        list_dicts.append(data_dict)
-    return list_dicts
-
-def generating_data_log_bootstraps(params_list, theta_list, 
-                                func, ns, pts_l, logs, ncpu=None):
-    '''
-    Generate data with bootstrapping. Creates a dictionary that maps the params
-    used to generate the fs (key) to a list of length 2 (value). The first
-    item in the list is the original fs data set. The second item in the list 
-    is a list (length 200) of the bootstrap data obtained by sampling from the 
-    original fs.
-    Note: does not take theta=1 since no sampling can be done on smooth data.
-    '''
-    arg_list = [(p, func, ns, pts_l, logs) for p in params_list]
-    with Pool(processes=ncpu) as pool:
-            fs_list = pool.map(worker_func_log, arg_list)
-
-    list_dicts = []
-    for theta in theta_list:
-        data_dict = {}
-        for params, fs in zip(params_list, fs_list):
-            fs_tostore = (theta*abs(fs)).sample()
-            data_dict[params] = [fs_tostore, []]
-            for i in range(200):
-                data_dict[params][1].append(fs_tostore.sample())       
-        list_dicts.append(data_dict)
-    return list_dicts
-
-def bootstrap_intervals(datafile, params, theta_i, percentile=95):
-    '''
-    pass a list of params as argument, e.g., [nu1, nu2, T, m]
-    '''
-    bs_results = pickle.load(open(datafile, 'rb'))
-    pred_theta = bs_results[theta_i] # dict of preds for fs scaled by theta with index [100, 1000, 10000]
-    keys = list(pred_theta.keys())
-    all_intervals_by_param = [[], [], [], []]
-    for j,key in enumerate(keys):
-        preds = pred_theta[key] # list of predictions from [orig fs pred, [200 boot fs pred]]
-        #print(f"true: {key}")
-        #print(f"orig: {preds[0]}")
-        bs_by_param = sort_by_param(preds[1])
-        bounds = (100 - percentile) / 2 / 100 
-        offset = int(len(bs_by_param[0]) * bounds) - 1 # e.g., 5th value is index 4
-        for i,cur_p in enumerate(params):
-            all_cur_p = bs_by_param[i] # sorted by current param, e.g., all nu1 preds sorted
-            all_cur_p.sort()
-            true = key[i]
-            orig = preds[0][i]
-            low = all_cur_p[offset]
-            high = all_cur_p[len(all_cur_p) - offset]
-            #print(f'---------{cur_p}---------')
-            #print(f'true: {true:.4f}')
-            #print(f'orig: {orig:.4f}')
-            #print(f'{percentile}% CI: {low:.4f}-{high:.4f}')
-            interval = [true, orig, low, high]
-            all_intervals_by_param[i].append(interval)
-    return all_intervals_by_param
-    # list length 4, inner list length 200, inner list length 4 [nu1, nu2, T, m]
-    # where nu1 = [list 1, list 2 ...] where list 1 = [true, orig, lower, upper]
-
-def log_transform_data(list_data_dict, num_list):
-    """
-    log transform specified params in input datasets
-    input data_dict has structure params:fs 
-    num_list is a number list e.g. [0,1] specifying 
-    the ith parameter to be log transformed
-    """
-    transformed_list_data_dict = []
-    for data_dict in list_data_dict:
-        transformed_data_dict = {}
-        params=data_dict.keys()
-        fs = [data_dict[params] for params in data_dict]
-        transformed_params = []
-        # loop through p in params, which is a list of param tuples
-        # for each tuple p, copy value if [i] is not in num_list,
-        # log transform value if [i] is in num_list
-        # store into tuple, then into transformed_param list
-        # need to convert tuple to a list because tuples
-        # are immutable
-        for p in params:
-            p = list(p)
-            for n in num_list:
-                p[n]=math.log10(p[n])
-                # p[n]=1/p[n] 
-            # convert list back to tuple for making dictionary keys
-            transformed_params.append(tuple(p))
-        transformed_data_dict=dict(zip(transformed_params, fs))
-        transformed_list_data_dict.append(transformed_data_dict)
-    return transformed_list_data_dict
-
-def un_log_transform_predict(y_predict, num_list):
-    # transformed_predict = y_predict.copy()
-    transformed_predict = copy.deepcopy(y_predict)
-    for p in transformed_predict:
-        for n in num_list:
-            p[n] = 10**p[n]
-    return transformed_predict
 
 def sort_by_param(y_true, y_pred):
     '''
@@ -197,7 +73,94 @@ def sort_by_param(y_true, y_pred):
         n+=1
     return param_true, param_pred
 
+def bootstrap_predictions(trained_model, bootstrap_data):
+    '''
+    Runs the ML predictions for all of the bootstrapped FS
+    trained_model is a single trained ML such as rfr_1000 used for prediction
+    bootstrap_data is a list of dictionaries of 200 items with the format 
+    {true_p:[orig_fs,[200 bootstrapped fs from orig_fs]]}
+    for each theta case in the list.
+    return: a list of bootstrap_pred dictionaries of the same
+    format as bootstrap_data
+    '''
+    bootstrap_pred = []
+    for theta_case in bootstrap_data:
+        pred_dict = {} # dictionary storing prediction results
+        for true_p, all_fs in theta_case.items():
+            # load elements from bootstrap data
+            orig_fs = all_fs[0] # single original fs
+            list_bstr_fs = all_fs[1] # list of 200 bootstrapped fs from orig_fs
+            # run prediction for the original fs
+            orig_fs = orig_fs/orig_fs.sum() # normalize for prediction
+            orig_fs_pred = trained_model.predict([orig_fs.flatten()]).flatten()
+            # run prediction for fs bootstrapped from original fs
+            list_bstr_fs_pred = []
+            for bstr_fs in list_bstr_fs:
+                bstr_fs = bstr_fs/bstr_fs.sum() # normalize before prediction
+                bstr_fs_pred = trained_model.predict([bstr_fs.flatten()]).flatten()
+                list_bstr_fs_pred.append(bstr_fs_pred)
+            # save all predictions into a dictionary for each theta case
+            pred_dict[true_p] = [orig_fs_pred, list_bstr_fs_pred]
+        # save all dictionaries into a list for multiple theta cases
+        bootstrap_pred.append(pred_dict)
+    return bootstrap_pred
+
+def bootstrap_intervals(bootstrap_pred_i, params, percentile=95):
+    '''
+    Generates a list of intervals sorted by param containing true value, 
+    original value, lower bound, and upper bound
+    bootstrap_pred_i: a single dict for 1 theta case 
+    from the list of dictionaries output from bootstrap_predictions()
+    params: list of params used in the model as strings, 
+    e.g, ['nu1', 'nu2', 'T', 'm']
+    theta_i: the index of desired theta to use from theta_list
+    percentile: the desired % confidence interval 
+    '''
+    keys = list(bootstrap_pred_i.keys()) # get dict keys into a list
+    all_intervals_by_param = [ [] for p in params] # list of lists to store results for each param
+
+    for j,key in enumerate(keys):
+        preds = bootstrap_pred_i[key] #format:[orig fs pred,[200 bstr_fs pred]]
+        #print(f"true: {key}")
+        #print(f"orig: {preds[0]}")
+        bstr_by_param = sort_by_param_bstr(preds[1]) # sort preds by param
+        bounds = (100 - percentile) / 2 / 100 
+        offset = int(len(bstr_by_param[0]) * bounds) - 1 # e.g., 5th value is index 4
+        for i,cur_p in enumerate(params):
+            all_cur_p = bstr_by_param[i] # sorted by current param, e.g., all nu1 preds sorted
+            all_cur_p.sort()
+            true = key[i]
+            orig = preds[0][i]
+            low = all_cur_p[offset]
+            high = all_cur_p[len(all_cur_p) - offset]
+            #print(f'---------{cur_p}---------')
+            #print(f'true: {true:.4f}')
+            #print(f'orig: {orig:.4f}')
+            #print(f'{percentile}% CI: {low:.4f}-{high:.4f}')
+            interval = [true, orig, low, high]
+            all_intervals_by_param[i].append(interval)
+    return all_intervals_by_param
+    # return is a list of length len(params), 
+    # with inner lists of length len(list_bstr_fs_pred), 
+    # with innermost list length 4.
+    # E.g., all_intervals_by_param = [nu1, nu2, T, m]
+    # where nu1=[list1,...,list200] where list1=[true, orig, lower, upper]
+
+def sort_by_param_bstr(p_sets):
+    '''
+    Sort by params for bootstrapping workflow.
+    '''
+    p_sorted = [] # list by param
+    for i in range(len(p_sets[0])):
+        p = [] # single param list (e.g., nu1)
+        for p_set in p_sets:
+            p.append(p_set[i])
+        p_sorted.append(p)
+    return p_sorted
+
 def msprime_two_epoch(s1, p):
+    '''Generate msprime demographic model equivalent to
+    two_epoch model from dadi with dadi parameters'''
     (nu, T) = p
     dem = msprime.Demography()
     dem.add_population(initial_size=s1*10**nu) # size at present time
@@ -207,6 +170,8 @@ def msprime_two_epoch(s1, p):
     return dem
 
 def msprime_generate_fs(args):
+    '''Simulate TS under msprime model, summarize as AFS
+    then convert to dadi FS'''
     (dem, ns, ploidy, seq_l, recomb, mut) = args
     # simuate tree sequences
     ts = msprime.sim_ancestry(samples=ns, ploidy=ploidy, demography=dem, 
@@ -232,7 +197,11 @@ def msprime_generate_fs(args):
         fs_tostore = fs/fs.sum()
     return fs_tostore
 
-def msprime_generate_data_parallel(params_list, dem_list, ns, ploidy, seq_l, recomb, mut, ncpu=None):
+def msprime_generate_data(params_list, dem_list, ns, ploidy, seq_l, recomb, mut, ncpu=None):
+    '''Parallelized version for generating data from msprime 
+    using multiprocessing.
+    Output format same as generate_data with dadi but FS were simulated
+    and summarized from TS data generated under msprime models.'''
     arg_list = [(dem, ns, ploidy, seq_l, recomb, mut) for dem in dem_list]
     with Pool(processes=ncpu) as pool:
         fs_list = pool.map(msprime_generate_fs, arg_list)
@@ -250,9 +219,13 @@ if __name__ == "__main__":
     import time
     import numpy as np
     import random
-    train_params = [(nu,T) for nu in 10**np.linspace(-2, 2, 10)
-                          for T in np.linspace(0.1,2,10)]
 
+    theta_list = [1, 1000]
+    func = dadi.Demographics1D.two_epoch
+    ns = [20]
+    pts_l = [40, 50, 60]
+
+    # testing running time for the generating_data functions (non log)
     test_params = []
     for i in range(100):
         nu = 10 ** (random.random() * 4 - 2)
@@ -260,13 +233,21 @@ if __name__ == "__main__":
         params = (round(nu, 2), round(T, 1))
         test_params.append(params)
     
-    theta_list = [1, 1000]
-    func = dadi.Demographics1D.two_epoch
-    ns = [20]
-    pts_l = [40, 50, 60]
-
-    # testing running time for the generating_data_parallel function
     start = time.time()
-    generating_data_parallel(train_params, theta_list, func, ns, pts_l)
+    generating_data(test_params, theta_list, func, ns, pts_l)
     print('Parallel execution time to generate data 1D: {0:.2f}s'
     .format(time.time()-start))
+
+    # test log implementation
+    test_params_log = []
+    while len(test_params_log) < 100:
+        nu = random.random() * 4 - 2
+        T = random.random() * 1.9 + 0.1
+        test_params_log.append((nu, T))
+
+    logs = [True,False]
+    start = time.time()
+    list_test_dict = generating_data(test_params_log, theta_list, func, ns, pts_l, logs)
+    print('Parallel execution time to generate data 1D log: {0:.2f}s'
+    .format(time.time()-start))
+    # print(list_test_dict[0].keys())
