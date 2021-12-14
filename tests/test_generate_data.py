529e8b8680fb526b287a32f52d5775f9cb2afee1
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-14 15:08:54 -0700

    Update test_generate_data.py

--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -7,3 +7,9 @@ def test_exists():
 
     PRG = '../src/generate_data.py'
     assert os.path.isfile(PRG)
+
+# test generate data for different demographic models
+
+# test generate data for multiple thetas
+
+# test generate data for bootstrap data
