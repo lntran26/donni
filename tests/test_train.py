6b6584f431742131c890369bdbc46337dfea99ec
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-14 15:08:57 -0700

    Create test_train.py

---
+++ tests/test_train.py
@@ -0,0 +1,7 @@
+""" Tests for train.py """
+
+# test if output model is a mapie regressor
+
+# test if output model make a sensible prediction
+
+# test mapie = False flag to generate regular mlpr with sklearn
