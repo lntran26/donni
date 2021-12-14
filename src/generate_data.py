ac08a5a2e22653144a0fdb598faeebbc39ba2354
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-14 12:05:11 -0700

    Update generate_data.py

--- src/generate_data.py
+++ src/generate_data.py
@@ -11,8 +11,13 @@ def worker_func(args):
     return func_ex(p, ns, pts_l)
 
 
-def generating_data(params_list, theta_list, func, ns, pts_l, logs=None,
-                    sample=True, norm=True, bootstrap=False, ncpu=None):
+# note: command-line: params_list will be made from specification
+# of number of params to generation, and upper bound and lower bound vals
+# ul and ll have to be specified for each dem param
+# or maybe rule will be included and specified for each dem model?
+def generate_data(theta_list, params_list, func, ns, pts_l,
+                  logs=None, sample=True, norm=True,
+                  bootstrap=False, n_bstr=200, ncpu=None):
     '''Parallelized version for generating data using multiprocessing.
     If npcu=None, it will use all the CPUs on the machine. 
     Otherwise user can specify a limit.
@@ -39,7 +44,7 @@ def generating_data(params_list, theta_list, func, ns, pts_l, logs=None,
             if bootstrap:  # only run this if want to generate bootstrap data sets
                 fs_tostore = (theta*abs(fs)).sample()
                 data_dict[params] = [fs_tostore, []]
-                for i in range(200):  # 200 bootstrap samples for each fs
+                for i in range(n_bstr):  # num bootstrap samples for each fs
                     data_dict[params][1].append(fs_tostore.sample())
             else:  # generate data for ML training and testing
                 fs.flat[0] = 0
@@ -58,3 +63,29 @@ def generating_data(params_list, theta_list, func, ns, pts_l, logs=None,
                     data_dict[params] = fs_tostore
         list_dicts.append(data_dict)
     return list_dicts
+
+
+if __name__ == "__main__":
+    # designate dadi demographic model, sample size, and extrapolation grid
+    func = dadi.Demographics1D.two_epoch
+    ns = [20]
+    pts_l = [40, 50, 60]
+    # specify param in log scale
+    logs = [True, False]
+
+    # generate params
+    params_list = []
+    while len(params_list) < 100:
+        # pick random values in specified range
+        # nu range: 0.01-100; T range: 0.1-2
+        log_nu = random.random() * 4 - 2
+        T = random.random() * 1.9 + 0.1
+        # if T/10**log_nu <= 5: # exclude certain T & nu combo
+        params_list.append((log_nu, T))
+
+    # generate data
+    data = generate_data(theta_list, params_list, func, ns, pts_l, logs)
+
+    # save data
+    pickle.dump(data, open(
+        '../tests/test_data/1d_2epoch_100fs_exclude', 'wb'), 2)
