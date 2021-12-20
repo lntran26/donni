e16e1777b3e2788dfc89ceb7beadd5db4d3e6f0d
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-20 12:06:11 -0700

    Update test_integration.py

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,12 +1,38 @@
 '''
 Intergration test for the entire program
 '''
-import os
+import re
 from subprocess import getstatusoutput, getoutput
 
-PRG = '../dadinet/__main__.py'
 
-def test_exists():
-    """ Program exists """
+# test program usage
+def test_usage():
+    """ Prints program usage """
 
-    assert os.path.isfile(PRG)
+    for flag in ['', '-h', '--help']:
+        out = getoutput(f'dadi-ml {flag}')
+        assert re.match("usage", out, re.IGNORECASE)
+
+
+# test subcommand usage
+def test_usage_subcommand():
+    """ Prints subcommand usage """
+
+    for subcommand in ['generate_data', 'train', 'predict', 'plot', 'tune']:
+        for flag in ['', '-h', '--help']:
+            out = getoutput(f'dadi-ml {subcommand} {flag}')
+            assert re.match("usage", out, re.IGNORECASE)
+
+
+# test generate_data subcommand
+def run_generate_data_sub(args):
+    """Template method for running generate_data subcommand"""
+    rv, out = getstatusoutput(f'dadi-ml generate_data {" ".join(args)}')
+
+    # check
+    assert ...
+
+
+def test_run_generate_data_sub_1():
+    ''''''
+    ...
