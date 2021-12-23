a7cb08f496b38ba018fa68bbf074259a7310d040
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-23 14:06:10 -0700

    update generate_data and tests to support only one theta and output dict
    all tests passed

--- dadinet/generate_data.py
+++ dadinet/generate_data.py
@@ -5,77 +5,70 @@ from multiprocessing import Pool
 import dadi
 
 
-def worker_func(args):
-    '''Helper function for generating_data()
-    Help with parallelization with Pool'''
+def worker_func(args: tuple):
+    '''
+    Helper function for generate_fs() to perform parallelization with Pool
+    '''
+
     (p, func, ns, pts_l) = args
     func_ex = dadi.Numerics.make_extrap_func(func)
     return func_ex(p, ns, pts_l)
 
 
-def generate_fs(func, params_list, logs, theta_list, ns, pts_l,
-                norm=True, bootstrap=False, n_bstr=200, ncpu=None):
+def generate_fs(func, params_list, logs: list[bool], theta: int, ns: int, pts_l,
+                norm=True, bootstrap=False, n_bstr=200, ncpu=None) -> dict:
+    '''
+    Parallelized generation of a dataset of multiple fs based on an input 
+    demographic model and a list of several demographic parameters
+    Inputs:
+        func: dadi demographic model
+        params_list: demographic model param sets
+        logs: indicate which dem param is in log10 values
+        theta: value of theta
+        ns: population sample size
+        pts_l: dadi extrapolation grid values
+        norm: whether to sample from and normalize the fs
+        bootstrap: whether to generate bootstrap data
+        n_bstr: number of bootstrap fs per original fs
+        n_cpu: integer num of CPUs to use for generating data
+            (None means using all)
+    Output: dataset dictionary with format params:fs
     '''
 
-
-
-    Parallelized version for generating data using multiprocessing.
-    If npcu=None, it will use all the CPUs on the machine.
-    Otherwise user can specify a limit (int).
-    logs is a list where len(logs) = len(p) and
-    logs[i] = True if p[i] is in log scale.
-    If bootstrap is True, will generate data for bootstrapping purpose.
-    Returns a list of dictionaries where each dictionary stores
-    a data set for training or testing RFR algorithm.
-    Data dictionaries have structure params:fs.
-    '''
     arg_list = [([10**p[i] if logs[i] else p[i] for i in range(len(logs))],
                  func, ns, pts_l) for p in params_list]
 
     with Pool(processes=ncpu) as pool:
         fs_list = pool.map(worker_func, arg_list)
 
+    data_dict = {}
+    for params, fs in zip(params_list, fs_list):
+        # mask corners of fs
+        fs.flat[0] = 0
+        fs.flat[-1] = 0
 
-    # data_dict = {}
-    list_dicts = []
-    for theta in theta_list:
-        data_dict = {}
-        for params, fs in zip(params_list, fs_list):
-            fs.flat[0] = 0
-            fs.flat[-1] = 0
-            if bootstrap:  # generate data for bootstrapping
-                fs_tostore = (theta*abs(fs)).sample()
-                data_dict[params] = [fs_tostore, []]
-                for i in range(n_bstr):  # num bootstrap samples for each fs
-                    data_dict[params][1].append(fs_tostore.sample())
+        # generate data for bootstrapping
+        if bootstrap:
+            fs_tostore = (theta*abs(fs)).sample()
+            data_dict[params] = [fs_tostore, []]
+            for i in range(n_bstr):  # num bootstrap samples for each fs
+                data_dict[params][1].append(fs_tostore.sample())
 
-            else:  # generate regular data
-                if theta == 1:
-                    fs_tostore = fs
-                else: # theta != 1 for noisy or non-normalized data
-                    if norm: # sample and normalize
-                        fs_tostore = (theta*abs(fs).sample())
-                        if fs_tostore.sum() == 0:  # check zero fs after sampling
-                            pass
-                        else:
-                            fs_tostore = fs_tostore/fs_tostore.sum()
-                    else: # no sampling and normalizing
-                        fs_tostore = (theta*abs(fs))
+        # generate regular data
+        else:
+            if theta == 1:
+                fs_tostore = fs
+            else:  # theta != 1 for noisy or non-normalized data
+                if norm:  # sample and normalize
+                    fs_tostore = (theta*abs(fs).sample())
+                    if fs_tostore.sum() == 0:  # check zero fs after sampling
+                        pass
+                    else:
+                        fs_tostore = fs_tostore/fs_tostore.sum()
+                else:  # no sampling and normalizing
+                    fs_tostore = (theta*abs(fs))
+
+            # save data
             data_dict[params] = fs_tostore
 
-
-                # if theta == 1:
-                #     fs_tostore = fs
-                # elif sample:
-                #     fs_tostore = (theta*abs(fs)).sample()
-                # else:
-                #     fs_tostore = (theta*abs(fs))
-                # if fs_tostore.sum() == 0:  # check non-zero fs
-                #     pass
-                # elif norm:
-                #     data_dict[params] = fs_tostore/fs_tostore.sum()
-                # else:
-                #     data_dict[params] = fs_tostore
-        list_dicts.append(data_dict)
-    # return data_dict
-    return list_dicts
+    return data_dict
--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -12,59 +12,66 @@ def test_exists():
     assert os.path.isfile(PRG)
 
 
-def run(model, sample_size, thetas, n_samples):
+def run(model, sample_size, theta, n_samples):
     '''Template method for testing different models, sample sizes, thetas'''
 
-    pts = [40, 50, 60]
+    grids = [40, 50, 60]
     dem, dem_params, p_logs = model(n_samples)
-    data = generate_fs(dem, dem_params, p_logs, thetas, sample_size, pts)
+    data = generate_fs(dem, dem_params, p_logs, theta, sample_size, grids)
 
-    # check that the method generates one data dict per theta value
-    assert len(data) == len(thetas)
+    # # check that the method generates one data dict per theta value
+    # assert len(data) == len(thetas)
 
-    # check that the number of FS in one data dict is as specified
-    assert len(data[0]) == n_samples
+    # check that each output dataset format is a dict
+    assert isinstance(data, dict)
 
-    # check that each dataset format is a dict
-    assert isinstance(data[0], dict)
+    # check that the number of FS in output dataset dict is as specified
+    assert len(data) == n_samples
 
+    all_fs = list(data.values())
     # check that the values in the data dict is a dadi FS object
-    assert isinstance(list(data[0].values())[0], dadi.Spectrum_mod.Spectrum)
+    # assert isinstance(list(data.values())[0], dadi.Spectrum_mod.Spectrum)
+    assert all(isinstance(fs, dadi.Spectrum_mod.Spectrum) for fs in all_fs)
 
     # check that the FS have the correct number of populations
-    assert len(list(data[0].values())[0].shape) == len(sample_size)
+    # assert len(list(data[0].values())[0].shape) == len(sample_size)
+    assert all(len(fs.shape) == len(sample_size) for fs in all_fs)
 
-    # check that the FS have the correct population sample size for each pop
-    for i, pop_size in enumerate(list(data[0].values())[0].shape):
-        assert pop_size == sample_size[i] + 1
+    # check that all FS have the correct population sample size for each pop
+    # for i, pop_size in enumerate(list(data[0].values())[0].shape):
+    for fs in all_fs:
+        for i, pop_size in enumerate(fs.shape):
+            assert pop_size == sample_size[i] + 1
 
+
+# add tests for theta != 1, norm and not norm
 
 def test_run_two_epoch():
     '''Generate three 10 FS datasets for the two_epoch model
     with one population sample size 20 and three thetas: 1, 1e4, 1e2'''
 
-    run(models.two_epoch, [20], [1, 10000, 100], 10)
+    run(models.two_epoch, [20], 1, 10)
 
 
 def test_run_growth():
     '''Generate two 20 FS datasets for the growth model
     with one population sample size 100 and two thetas: 1 and 1e3'''
 
-    run(models.growth, [100], [1, 1000], 20)
+    run(models.growth, [100], 1000, 20)
 
 
 def test_run_split_mig():
     '''Test generate one 5 FS datasets for the split migration model
     with two populations sample sizes 10, 15 and theta 100'''
 
-    run(models.split_mig, [10, 15], [100], 5)
+    run(models.split_mig, [10, 15], 100, 5)
 
 
 def test_run_IM():
     '''Test generate one 3 FS datasets for the IM model
     with two populations sample sizes 30, 21 and theta 1000'''
 
-    run(models.IM, [30, 21], [1000], 3)
+    run(models.IM, [30, 21], 10000, 3)
 
 
 # test generate data for bootstrap data
