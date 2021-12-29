390012094a9ac9184f59edac0b0e8f3437115ba2
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-29 13:18:39 -0700

    provide example codes to use pipeline

---
+++ examples/generate_data_split_mig_20_20.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dadi-ml generate_data --model split_mig --n_samples 1500 --sample_sizes 20 20 --outfile data/split_mig_500
---
+++ examples/generate_data_two_epoch_20.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dadi-ml generate_data --model two_epoch --n_samples 500 --sample_sizes 20 --outfile data/two_epoch_500
