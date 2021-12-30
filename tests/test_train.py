3746128d1144a0b3f70401eec69a454c227d9bea
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-12-29 23:34:27 -0700

    Update test_train.py
    start writing tests for train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,7 +1,111 @@
 """ Tests for train.py """
+import os
+import pickle
+import math
+from sklearn.experimental import enable_halving_search_cv  # noqa
+from sklearn.model_selection import HalvingRandomSearchCV
+from sklearn.utils.fixes import loguniform
+from dadinet.train import prep_data, tune  # , report,\
+#    get_best_specs, train, get_cv_score
 
-# test if output model is a mapie regressor
 
-# test if output model make a sensible prediction
+def test_exists():
+    """ Test program exists """
 
-# test mapie = False flag to generate regular mlpr with sklearn
+    PRG = '../dadinet/train.py'
+    assert os.path.isfile(PRG)
+
+
+def test_prep_data():
+    """ Test prep_data() method """
+
+    for data_file in ['two_epoch_500', 'split_mig_1500']:
+        data = pickle.load(open(f'test_data/{data_file}', 'rb'))
+        X, y = prep_data(data)
+        # test that X has the correct n_samples
+        assert len(X) == len(data)
+        # test that y contains the same number of inner list per dem param
+        assert any(len(key) == len(y) for key in list(data.keys()))
+        # test that each inner list in y has the correct n_samples
+        assert all(len(y_unpack) == len(X) for y_unpack in y)
+
+        # test using sklearn instead of mapie
+        X, y = prep_data(data, mapie=False)
+        # test that X has the correct n_samples
+        assert len(X) == len(data)
+        # test that y contains only 1 inner list
+        assert len(y) == 1
+        # test that the inner list in y has the correct n_samples
+        assert len(y[0]) == len(X)
+        # test that each member of the inner list contains the
+        # correct number of dem params
+        for val, key in zip(y[0], list(data.keys())):
+            assert len(val) == len(key)
+
+
+def run_tune(data_file, param_dist, max_iter=243, eta=3, cv=5):
+    """ Template method for testing tune() method """
+    data = pickle.load(open(f'{data_file}', 'rb'))
+    X, y = prep_data(data)
+    all_results = tune(X, y, param_dist, max_iter, eta, cv)
+    # check that we have one result per mlpr label
+    assert len(all_results) == len(y)
+    # check that each band within hyperband for each model
+    # is a HalvingRandomSearchCV object (successive halving)
+    for each_model in all_results:
+        assert all(isinstance(each_band, HalvingRandomSearchCV)
+                   for each_band in each_model)
+    # check that each model has the correct number of bands
+    s_max = int(math.log(max_iter)/math.log(eta))
+    assert all(len(results) == s_max+1 for results in all_results)
+
+
+def test_run_tune1():
+    ''' Test tuning with two_epoch'''
+    data = 'test_data/two_epoch_500'
+    param_dist = {'hidden_layer_sizes': [(64,), (64, 64), (128,)],
+                  'activation': ['tanh', 'relu'],
+                  'solver': ['lbfgs', 'adam'],
+                  'alpha': loguniform(1e-4, 1e0),
+                  'tol': loguniform(1e-4, 1e-2),
+                  'early_stopping': [False, True]}
+    run_tune(data, param_dist)
+
+
+def test_run_tune2():
+    ''' Test tuning with split_mig'''
+    data = 'test_data/split_mig_1500'
+    param_dist = {'hidden_layer_sizes': [(64,), (64, 64), (128,)],
+                  'activation': ['tanh', 'relu'],
+                  'solver': ['lbfgs', 'adam'],
+                  'alpha': loguniform(1e-4, 1e0),
+                  'tol': loguniform(1e-4, 1e-2),
+                  'early_stopping': [False, True]}
+    run_tune(data, param_dist)
+
+
+def run_train():
+    """ Template method for testing train() method """
+    ...
+
+    # test if output model is a mapie regressor
+
+    # test if output model has the specified specs
+    # # for debugging only
+    # print(mlpr.get_params())
+
+    # test if output model make a sensible prediction
+
+    # test mapie = False flag to generate regular mlpr with sklearn
+
+
+# if __name__ == "__main__":
+
+#     mlpr_param = {'hidden_layer_sizes': (100,),
+#                   'activation': 'relu', 'solver': 'lbfgs',
+#                   'alpha': 1, 'max_iter': 5000}
+#     data = '../tests/test_data/1d_2epoch_100fs_exclude'
+#     ml_model = '../out'
+
+#     # train(mlpr_param, ml_model, data)
+#     train(mlpr_param, ml_model, data, mapie=False)
