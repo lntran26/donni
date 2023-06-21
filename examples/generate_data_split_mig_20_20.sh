07c141b9c286568412b8e723d1a4a4fd5a6d0f6e
Linh Tran <38679445+lntran26@users.noreply.github.com>
2023-06-20 21:53:01 -0700

    Removed old files

--- examples/generate_data_split_mig_20_20.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dadi-ml generate_data --model split_mig --n_samples 1500 --sample_sizes 20 20 --outfile data/split_mig_1500
--- examples/generate_data_two_epoch_20.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dadi-ml generate_data --model two_epoch --n_samples 500 --sample_sizes 20 --outfile data/two_epoch_500
